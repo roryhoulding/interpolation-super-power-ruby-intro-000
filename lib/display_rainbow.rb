23f540e72c4932a0eede3113fe8628c450743f0b
Rory Houlding <rohoulding@googlemail.com>
2019-09-01 16:55:17 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -5,4 +5,4 @@ def display_rainbow(colors)
   puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[2]}, B: #{colors[3]}, I: #{colors[4]}, V: #{colors[5]}"
 end
 
-display_rainbow
+display_rainbow(colors)
